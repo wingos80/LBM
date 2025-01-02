04d2002dd401b2b2f50c11cab8d506b6e69fd607
wingos80 <wingyc80@gmail.com>
2025-01-02 16:32:50 +0100

    [Refactor] Refactored, added todo list

--- main.py
+++ main.py
@@ -1,82 +1,76 @@
-import numpy as np
-import os
-from settings import *
-os.environ["JAX_PLATFORM_NAME"] = USE_DEVICE
+"""
+MIT License
 
-from jnpfuncs import *
+Copyright (c) 2025 Wing Yin Chan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import numpy as np
+from settings import *
+if USE_LIBRARY==JAX:
+    from jnpfuncs import *
+elif USE_LIBRARY==NP:
+    from npfuncs import *
 from utils import *
 from tqdm import tqdm
 
-print(f"You are using device: {jax.devices()[0]}")
+if __name__ == "__main__":
+    # Pressure difference?
+    calculate_f_eq_out = lambda u_1: calculate_f_eq(rho_out, u_1)[-1,:,:]
+    calculate_f_eq_in = lambda u_N: calculate_f_eq(rho_in, u_N)[0,:,:]
 
-# TODO have all libarry imports in one place instead of scattered between e.g. main.py, settings.py, utils.py...?
-# TODO add saving class/context manager/something to make saving pictures prettier
-# TODO redo folder struction
-# TODO better solids creation/customization
-# TODO add functionality to select D2Q9 or supply own velocity set (change moment and f_eq calcs to generic)
+    # Initial Conditions
+    # f = flow_taylorgreen()
+    # f = flow_up()
+    f = flow_right()
+    solids = create_solids()
 
-# Pressure difference
-calculate_f_eq_out = lambda u_1: calculate_f_eq(rho_out, u_1)[-1,:,:]
-calculate_f_eq_in = lambda u_N: calculate_f_eq(rho_in, u_N)[0,:,:]
-
-# Initial Conditions
-# f = flow_taylorgreen()
-# f = flow_up()
-f = flow_right()
-solids = create_solids()
-
-@jax.jit
-def one_time_march(f: np.ndarray):
-    """
-    Simulate one step forward in time
-    """
-    # # # absorbing left and right walls
-    # # f[0,:,[1,5,8]] = f[1,:,[1,5,8]]
-    # # f[-1,:,[3,6,7]] = f[-2,:,[3,6,7]]
-    rho = calculate_rho(f)
-    momentum = calculate_momentum(f)
-    u = calculate_velocity(momentum, rho)
-
-    # f_eq_in = calculate_f_eq_out(u[0,:,:].reshape(-1,YMAX,2))
-    # f_eq_out = calculate_f_eq_out(u[-1,:,:].reshape(-1,YMAX,2))
-    f_eq = calculate_f_eq(rho, u)
-    f = collision_step(f, f_eq, dt=DT, tau=TAU)
-    # f_star[0,:,:] = f_eq_in + f_star[-2,:,:] - f_eq[-2,:,:]
-    # f_star[-1,:,:] = f_eq_out + f_star[1,:,:] - f_eq[1,:,:]
-    f = streaming_step(f)
-    f = BC_solids(f, solids)
-    return f, u
-
-
-with catchtime() as timer:
-    if not RECORD:
-        print("Rendering simulation")
-        for t in tqdm(range(TIME)):
-            f, u = one_time_march(f)
-        
-            # plot in real time
-            if t%100==0:
-                vorticity = (np.roll(u[:,:,0], -1, axis=1) - np.roll(u[:,:,0], 1, axis=1)) - (np.roll(u[:,:,1],-1,axis=0) - np.roll(u[:,:,1],1,axis=0))
-                vorticity[solids] = np.nan
-                plot(vorticity.T)
-    elif RECORD:
-        print(f"Recording simulation, saving frames to ./recording/{USE_DEVICE}/{IMG_TYPE}/")
+    with catchtime() as timer:
         start_time = time.time()
         elapsed_time = time.time() - start_time
-        last_saved = elapsed_time
-        frame = 0
-        while (elapsed_time < RECORD_TIME):
-            elapsed_time = time.time() - start_time
-            f, u = one_time_march(f)
-
-            time_since_last_saved = elapsed_time - last_saved
-            print(f"{elapsed_time:.3f} s: Time since last saved: {time_since_last_saved:.3f} s", end="\r")
-            # save frames every fps seconds
-            if time_since_last_saved > FT:
-                    frame += 1
+        if not RECORD:
+            print("Rendering simulation")
+            for t in tqdm(range(TIME)):
+                f, u = one_time_march(f, solids)
+                
+                # plot every PLOT_EVERY seconds
+                if t % PLOT_EVERY == 0:
                     vorticity = (np.roll(u[:,:,0], -1, axis=1) - np.roll(u[:,:,0], 1, axis=1)) - (np.roll(u[:,:,1],-1,axis=0) - np.roll(u[:,:,1],1,axis=0))
                     vorticity[solids] = np.nan
-                    plot(vorticity.T, frame=frame, save_dir=USE_DEVICE, img_type=IMG_TYPE, save=True)
-                    last_saved = time.time() - start_time
-        print("\n")
+                    plot(vorticity.T)
+        elif RECORD:
+            print(f"Recording simulation, saving frames to ./recording/{USE_DEVICE}/{IMG_TYPE}/")
+            last_saved = elapsed_time
+            frame = 0
+            while (elapsed_time < RECORD_TIME):
+                elapsed_time = time.time() - start_time
+                f, u = one_time_march(f, solids)
+
+                time_since_last_saved = elapsed_time - last_saved
+                print(f"{elapsed_time:.3f} s: Time since last saved: {time_since_last_saved:.3f} s", end="\r")
+                # save frames every FT seconds
+                if time_since_last_saved > R_FT:
+                        frame += 1
+                        vorticity = (np.roll(u[:,:,0], -1, axis=1) - np.roll(u[:,:,0], 1, axis=1)) - (np.roll(u[:,:,1],-1,axis=0) - np.roll(u[:,:,1],1,axis=0))
+                        vorticity[solids] = np.nan
+                        plot(vorticity.T, frame=frame, save_dir=USE_DEVICE, img_type=IMG_TYPE, save=True)
+                        last_saved = time.time() - start_time
+            print("\n")
 
--- settings.py
+++ settings.py
@@ -1,32 +1,40 @@
+# Settings
+JAX = "jax"
+NP = "numpy"
+GPU = "gpu"
+CPU = "cpu"
+
+# Select library and device
+USE_LIBRARY = JAX
+USE_DEVICE  = GPU
+
 # Grid size
-XMAX = 400
-YMAX = 100
+XMAX  = 400
+YMAX  = 100
 MSIZE = XMAX*YMAX
 
 # Control variables
-TAU = 0.53
-DT = 1
-CS2 = 1/1
-TIME = 16000
+TAU  = 0.53
+DT   = 1
+CS2  = 1/1
+TIME = 1000
 
-# Rendering options
-RECORD = False
+# Plotting options
+PLOT_EVERY = 100  # render every n-th frame
+
+# Recording options
+RECORD      = False  # toggle recording
 RECORD_TIME = 5  # how many seconds to record for
-FPS = 15  # frames per second
-FT = 1/FPS  # frame time
-IMG_TYPE = "png"
-
-# Jax options
-CPU = "cpu"
-GPU = "gpu"
-USE_DEVICE = CPU
+R_FPS       = 15  # frames per second
+R_FT        = 1/R_FPS  # frame time
+IMG_TYPE    = "png"
 
 # Pressure difference
-DP = 0.1
-P_OUT = 1
+DP      = 0.1
+P_OUT   = 1
 rho_out = P_OUT/CS2
-p_in = P_OUT + DP
-rho_in = p_in/CS2
+p_in    = P_OUT + DP
+rho_in  = p_in/CS2
 
 # D2Q9 velocity set
 import numpy as np
@@ -38,3 +46,16 @@ x = np.arange(XMAX)
 y = np.arange(YMAX)
 X,Y = np.meshgrid(x,y)
 X,Y = X.T, Y.T
+
+
+class COLOR:
+   PURPLE = '\033[1;35;48m'
+   CYAN = '\033[1;36;48m'
+   BOLD = '\033[1;37;48m'
+   BLUE = '\033[1;34;48m'
+   GREEN = '\033[1;32;48m'
+   YELLOW = '\033[1;33;48m'
+   RED = '\033[1;31;48m'
+   BLACK = '\033[1;30;48m'
+   UNDERLINE = '\033[4;37;48m'
+   END = '\033[1;37;0m'
