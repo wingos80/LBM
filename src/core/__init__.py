b8d62114a7d69077a086f59527ca93879fe5299f
wingos80 <wingyc80@gmail.com>
2025-05-05 20:35:44 +0200

    [Improve] Updated log statements

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,10 +1,6 @@
-print("in core init")
 import logging
 from settings import *
 import platform
-
-# # Uncomment this to expose all logs from imported libraries
-# logging.basicConfig(level=logging.WARNING, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",)
 
 # Defining the default logger
 logger = logging.getLogger(__name__)
@@ -14,7 +10,7 @@ logger.info("core initialized")
 # Create handler
 handler = logging.StreamHandler()
 
-# Custom formatter to apply green only to INFO messages
+# Custom formatter to apply color to log statements
 class ColorFormatter(logging.Formatter):
     def format(self, record):
         message = super().format(record)
@@ -28,7 +24,7 @@ class ColorFormatter(logging.Formatter):
         return message
 
 formatter = ColorFormatter(
-    fmt="%(asctime)s [%(levelname)s] %(filename)s:line %(lineno)d >>> %(message)s"
+    fmt="%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d >>> %(message)s"
 )
 handler.setFormatter(formatter)
 
@@ -38,13 +34,10 @@ if not logger.hasHandlers():
 
 if USE_LIBRARY=="jax":
     if USE_DEVICE=="gpu" and platform.system()=="Windows":
-        logger.warning(f"JAX on windows does not have GPU support")
+        logger.warning(f"JAX on windows does not have GPU support but was selected")
         USE_DEVICE = "cpu"
 elif USE_LIBRARY=="numpy":
     if USE_DEVICE=="gpu":
         logger.warning(f"Numpy does not have GPU support")
         USE_DEVICE = "cpu"
-# else:
-#     logger.error(f"Unknown library: {USE_LIBRARY=}, defaulting to JAX with CPU")
-#     USE_LIBRARY = "jax"
-#     USE_DEVICE = "cpu"
+        
